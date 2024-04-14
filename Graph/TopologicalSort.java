8b12304963b07d8ea8aa75b1e66520390205ace0
Bhautik Sosa <158452267+bhautik-sosa@users.noreply.github.com>
2024-04-14 14:00:42 +0530

    Create TopologicalSort.java
    Given a Directed Acyclic Graph (DAG) with V vertices and E edges, Find any Topological Sorting of that Graph.
    
    Example 1:
    
    Input:
    
    Output:
    1
    Explanation:
    The output 1 denotes that the order is
    valid. So, if you have, implemented
    your function correctly, then output
    would be 1 for all test cases.
    One possible Topological order for the
    graph is 3, 2, 1, 0.
    
    
    Expected Time Complexity: O(V + E).
    Expected Auxiliary Space: O(V).
    
    Constraints:
    2 ≤ V ≤ 104
    1 ≤ E ≤ (N*(N-1))/2

---
+++ Graph/TopologicalSort.java
@@ -0,0 +1,40 @@
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj){
+        
+        // -----> Kahn's Algorithm for Topological Sort :
+        
+        int[] inDegree = new int[V];
+        Arrays.fill( inDegree , 0);
+        
+        for( int i=0 ; i < V ; i++){
+            
+            for( int it : adj.get(i)){
+                inDegree[it]++;
+            }
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for( int i=0 ; i < V ; i++){
+            if( inDegree[i] == 0 ){
+                q.offer(i);
+            }
+        }
+        
+        int[] topo = new int[V];
+        int ind =0;
+        while( !q.isEmpty()){
+            int node = q.poll();
+            
+            topo[ind++] = node;
+            for( int it : adj.get(node)){
+                inDegree[it]--;
+                if( inDegree[it] == 0 ) q.offer(it);
+            }
+        }
+        
+        return topo;
+    }
+
+}
